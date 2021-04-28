ce343e69c1c2b337b0467a2a86191420128775e8
Scott Guymer <scott.guymer@philips.com>
2021-04-28 11:30:38 +0200

    Updated dotnet app to 3.1

--- services/dotnetcore/Startup.cs
+++ services/dotnetcore/Startup.cs
@@ -1,43 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System.Collections;
-
+﻿  using Microsoft.AspNetCore.Builder;
+  using Microsoft.AspNetCore.Hosting;
+  using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.Extensions.Hosting;
 namespace dotnetcore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            this.loggerFactory = loggerFactory;
         }
 
         public IConfiguration Configuration { get; }
-        private ILoggerFactory loggerFactory;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {           
-            services.AddMvc();
+           services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
+            } 
+            app.UseRouting();
+            app.UseEndpoints(
+              endpoints =>
+              {
+                endpoints.MapControllers();
+              });
         }
     }
 }
