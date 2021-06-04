d20dc17f31df36c26731fcdda08672d65c671d18
Scott Guymer <scott.guymer@philips.com>
2021-06-04 15:13:22 +0200

    Remove un-used code

--- services/java/src/main/java/springdemo/ServiceController.java
+++ services/java/src/main/java/springdemo/ServiceController.java
@@ -3,27 +3,16 @@ package springdemo;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.*;
-import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class ServiceController {
-
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
-  
-
-  private @Autowired HttpServletRequest request;
 
   @RequestMapping("/{service}")
   public WrappedResponse get(@PathVariable("service") String service) {
@@ -50,8 +39,6 @@ public class ServiceController {
     map.forEach((key,value) -> con.setRequestProperty(key, value));
     
 		con.setRequestMethod("GET");
-
-		int responseCode = con.getResponseCode();
 
 		BufferedReader in = new BufferedReader(
 		        new InputStreamReader(con.getInputStream()));
